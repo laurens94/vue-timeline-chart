65158892592429bd5090ec5bdeff35f79320ddc2
Laurens <laurens94@users.noreply.github.com>
2025-11-22 07:36:43 +1200

    🚑 Fix style.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
 
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: ['src/index.ts'],
       name: 'vue-timeline-chart',
       fileName: 'vue-timeline-chart',
+      cssFileName: 'style',
     },
     rollupOptions: {
       external: ['vue'],
