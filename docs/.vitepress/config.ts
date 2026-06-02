3b0406c781023c54713434cf0c323e2e7895324c
Laurens <laurens94@users.noreply.github.com>
2026-06-02 15:37:53 +0200

    📝 Add favicon to docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,13 +1,14 @@
 import { transformerTwoslash } from '@shikijs/vitepress-twoslash';
 import { defineConfig } from 'vitepress'
 
+const base = '/vue-timeline-chart/';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Vue Timeline Chart",
   description: "Documentation",
-  base: '/vue-timeline-chart/',
+  base,
   head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/vue-timeline-chart/favicon.svg' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: `${base}favicon.svg` }],
     ['script', { src: 'https://unpkg.com/d3@7.9.0/dist/d3.min.js', integrity: 'sha384-CjloA8y00+1SDAUkjs099PVfnY2KmDC2BZnws9kh8D/lX1s46w6EPhpXdqMfjK6i', crossorigin: 'anonymous', defer: 'true' }],
     ['script', { src: 'https://unpkg.com/@popperjs/core@2.11.8/dist/umd/popper.min.js', integrity: 'sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r', crossorigin: 'anonymous', defer: 'true' }],
     ['script', { src: 'https://unpkg.com/tippy.js@6.3.7/dist/tippy-bundle.umd.min.js', integrity: 'sha384-AiTRpehQ7zqeua0Ypfa6Q4ki/ddhczZxrKtiQbTQUlJIhBkTeyoZP9/W/5ulFt29', crossorigin: 'anonymous', defer: 'true' }],
